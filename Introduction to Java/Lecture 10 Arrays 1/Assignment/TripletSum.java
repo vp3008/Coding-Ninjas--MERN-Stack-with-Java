0d6e9a34270bc0aa34ae11a897b48dad6e80f2fe
Deepankar Pathak <vp3008@gmail.com>
2020-09-21 21:58:00 +0530

    Corrected Triplet Sum code

--- Introduction to Java/Lecture 10 Arrays 1/Assignment/TripletSum.java
+++ Introduction to Java/Lecture 10 Arrays 1/Assignment/TripletSum.java
@@ -13,22 +13,23 @@ Explanation for Input 2:Since there doesn'texist any triplet with sum equal to 1
 For the second query,we have 5 triplets in total that sum up to 10. They are,(2,8,0),(2,11,-3),(-5,5,10),(8,5,-3)and(-6,5,11) */
 
 
+
 public class TripletSum {
-
+    
     public static int findTriplet(int[] arr, int x) {
-        // Your code goes here
+    	//Your code goes here
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
-        
-
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                for(int k=j+1; k<arr.length; k++){
+                    if(arr[i]+arr[j]+arr[k] == x)
+                        count++;
+                }
+            }
         }
-
         return count;
-
-     
-} 
+    }
+}
         
 
             
